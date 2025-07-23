0de88dbb6671220862701b4ec702fad8b2aae7ff
Carlos Villanueva <cs.vp@outlook.com>
2025-07-23 11:44:21 -0600

    feat(customer): add new batch method to get products info using a rest client

--- business-domain/customer/src/main/java/com/paymentchain/customer/client/ProductRestClientService.java
+++ business-domain/customer/src/main/java/com/paymentchain/customer/client/ProductRestClientService.java
@@ -1,7 +1,9 @@
 package com.paymentchain.customer.client;
 
 import com.paymentchain.customer.dto.ProductDTO;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
@@ -13,5 +15,14 @@ public class ProductRestClientService {
 
   public ProductDTO getProductById(Long id) {
     return productRestClient.get().uri("/{id}", id).retrieve().body(ProductDTO.class);
+  }
+
+  public List<ProductDTO> getProductsByIds(List<Long> productIds) {
+    return productRestClient
+        .post()
+        .uri("/batch")
+        .body(productIds)
+        .retrieve()
+        .body(new ParameterizedTypeReference<>() {});
   }
 }
