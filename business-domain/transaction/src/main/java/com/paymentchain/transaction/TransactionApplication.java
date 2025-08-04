bc94deaf6cfbfaa473e2991b117a7eb61c94522f
Carlos Villanueva <cs.vp@outlook.com>
2025-08-04 12:09:38 -0600

    style(transaction): enforce consistent code style across transaction project
    apply spotless plugin with google java format
    add plugin execution goals to check and apply changes

--- business-domain/transaction/src/main/java/com/paymentchain/transaction/TransactionApplication.java
+++ business-domain/transaction/src/main/java/com/paymentchain/transaction/TransactionApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TransactionApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TransactionApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(TransactionApplication.class, args);
+  }
 }
