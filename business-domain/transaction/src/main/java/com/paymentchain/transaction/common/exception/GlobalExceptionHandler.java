58e2186ef2b8194b372e8db2a1c8d1aa4e4736e9
Carlos Villanueva <cs.vp@outlook.com>
2025-08-07 20:31:15 -0600

    feat(transaction): add new methods to handle different kind of ServletRequest Binding Exceptions

--- business-domain/transaction/src/main/java/com/paymentchain/transaction/common/exception/GlobalExceptionHandler.java
+++ business-domain/transaction/src/main/java/com/paymentchain/transaction/common/exception/GlobalExceptionHandler.java
@@ -1,14 +1,20 @@
 package com.paymentchain.transaction.common.exception;
 
+import java.util.Collections;
 import java.util.Map;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingPathVariableException;
+import org.springframework.web.bind.MissingRequestHeaderException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.ServletWebRequest;
@@ -63,5 +69,65 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     return ResponseEntity.status(BusinessExceptionReason.ARGUMENT_NOT_VALID_ERROR.getStatus())
         .body(problemDetail);
+  }
+
+  @Override
+  protected ResponseEntity<Object> handleServletRequestBindingException(
+      final ServletRequestBindingException ex,
+      final HttpHeaders headers,
+      final HttpStatusCode status,
+      final WebRequest request) {
+
+    final String missingParameter;
+    final String missingParameterType;
+
+    switch (ex) {
+      case MissingRequestHeaderException headerException -> {
+        missingParameter = headerException.getHeaderName();
+        missingParameterType = "header";
+      }
+      case MissingServletRequestParameterException parameterException -> {
+        missingParameter = parameterException.getParameterName();
+        missingParameterType = "query";
+      }
+      case MissingPathVariableException pathVariableException -> {
+        missingParameter = pathVariableException.getVariableName();
+        missingParameterType = "path";
+      }
+      default -> {
+        missingParameter = "unknown";
+        missingParameterType = "unknown";
+      }
+    }
+
+    String value =
+        String.format(
+            "Missing %s parameter with name '%s'", missingParameterType, missingParameter);
+
+    final ProblemDetail problemDetail =
+        new ProblemDetailBuilder(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage())
+            .errors(Collections.singletonMap(missingParameter, value))
+            .build();
+
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problemDetail);
+  }
+
+  @Override
+  protected ResponseEntity<Object> handleMissingServletRequestParameter(
+      final MissingServletRequestParameterException ex,
+      final HttpHeaders headers,
+      final HttpStatusCode status,
+      final WebRequest request) {
+
+    return handleServletRequestBindingException(ex, headers, status, request);
+  }
+
+  @Override
+  protected ResponseEntity<Object> handleMissingPathVariable(
+      final MissingPathVariableException ex,
+      final HttpHeaders headers,
+      final HttpStatusCode status,
+      final WebRequest request) {
+    return handleServletRequestBindingException(ex, headers, status, request);
   }
 }
